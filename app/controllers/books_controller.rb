46faf90fa768e7662af1cd266e47570ce8cc5ce6
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-06-08 05:28:46 +0000

    [Add]/index.htmlバリデーション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to action: :index
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
+    else
+       @books = Book.all
+       render :index
+    end
   end
 
   def show
@@ -23,8 +27,11 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+       redirect_to book_path(book.id)
+    else
+       render :edit
+    end
   end
 
   def destroy
